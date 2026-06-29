09904d527e6c1e80c3f297193a4a9b4fc865c504
vanessaAbigail <abimartinez100817@gmail.com>
2026-06-29 10:37:01 -0600

    Corregir conexión MySQL para XAMPP y Aiven

--- server.js
+++ server.js
@@ -55,9 +55,17 @@ transporter.verify(function(error, success){
 // 🔥 CONEXIÓN BD
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "planeacion",
+  ...(process.env.DB_HOST
+    ? {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {}),
 });
 
 
