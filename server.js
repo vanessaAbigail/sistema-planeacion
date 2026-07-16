112157bd42cb9be754131cfe3123a421e2089a26
vanessaAbigail <abimartinez100817@gmail.com>
2026-07-16 14:34:43 -0600

    Configurar correo con variables de Render

--- server.js
+++ server.js
@@ -44,13 +44,11 @@ const transporter = nodemailer.createTransport({
 
   auth: {
 
-    auth: {
+    user: process.env.EMAIL_USER,
 
- user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
 
- pass: process.env.EMAIL_PASS
-
-},
+  },
 
   tls:{
     rejectUnauthorized:false
